3583421459b2b51d15f71b2ea1d3fc8bf26165de
Italo Siqueira Lima <italosiqueira@gmail.com>
2024-11-20 13:16:25 -0300

    [feat] Adiciona IDs aos posts e cria endpoint de busca por ID

--- server.js
+++ server.js
@@ -2,22 +2,27 @@ import express from "express";
 
 const posts = [
     {
+        id: 1,
         descricao: "Neo test image",
         imagem: "https://placecats.com/neo/200/300"
     },
     {
+        id: 2,
         descricao: "Millie test image",
         imagem: "https://placecats.com/millie/200/300"
     },
     {
+        id: 3,
         descricao: "Bella test image",
         imagem: "https://placecats.com/bella/200/400"
     },
     {
+        id: 4,
         descricao: "Neo Banana test image",
         imagem: "https://placecats.com/neo_banana/150/300"
     },
     {
+        id: 5,
         descricao: "General test image",
         imagem: "https://placecats.com/g/200/300"
     }
@@ -35,3 +40,14 @@ app.get("/api", (req, res) => {
 app.get("/posts", (req, res) => {
     res.status(200).json(posts);
 });
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id);
+    res.status(200).json(posts[index]);
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
